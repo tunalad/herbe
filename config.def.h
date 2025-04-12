30a94b140270a502fc87ae0377b774bb929f6cba
tunalad <28594843+tunalad@users.noreply.github.com>
2025-04-12 21:30:43 +0200

    changed font sizing and notification positioning

--- config.def.h
+++ config.def.h
@@ -1,14 +1,14 @@
 static const char *background_color = "#3e3e3e";
 static const char *border_color = "#ececec";
 static const char *font_color = "#ececec";
-static const char *font_pattern = "monospace:size=10";
+static const char *font_pattern = "monospace:size=8";
 static const unsigned line_spacing = 5;
-static const unsigned int padding = 15;
+static const unsigned int padding = 8;
 
-static const unsigned int width = 450;
+static const unsigned int width = 300;
 static const unsigned int border_size = 2;
-static const unsigned int pos_x = 30;
-static const unsigned int pos_y = 60;
+static const unsigned int pos_x = 15;
+static const unsigned int pos_y = 55;
 
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
 enum corners corner = TOP_RIGHT;
